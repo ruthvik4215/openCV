2cdb3bf151e52dbb715dd57b7f253b3e39a0e5de
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-02-16 22:51:48 +0530

    Update imageInvertion.py

--- scripts/imageInvertion.py
+++ scripts/imageInvertion.py
@@ -1,24 +1,31 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+liveFootage = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
+    retx_x, frame = liveFootage.read()
+    
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    image = np.zeros(frame.shape, np.uint8)
-    smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
-    image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
-    image[height//2:, width//2:] = smaller_frame
+    image_size = np.zeros(frame.shape, np.uint8)
+    
+    resizing_frame_into_four = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    
+    image_size[:height//2, :width//2] = cv2.rotate(resizing_frame_into_four, cv2.cv2.ROTATE_180)
+    
+    image_size[height//2:, :width//2] = resizing_frame_into_four
+    
+    image_size[:height//2, width//2:] = cv2.rotate(resizing_frame_into_four, cv2.cv2.ROTATE_180)
+    
+    image_size[height//2:, width//2:] = resizing_frame_into_four
 
-    cv2.imshow('frame', image)
+    cv2.imshow('frame', image_size)
 
     if cv2.waitKey(1) == ord('q'):
         break
 
 cap.release()
+
 cv2.destroyAllWindows()
