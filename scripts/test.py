fa32151ed16406f3d18764f2deee376bce182fa1
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-04-20 20:08:49 +0530

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -1,16 +1,16 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('assets/chessboard.png')
-img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
-gray_scaled_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_asset = cv2.imread('assets/chessboard.png')
+img_asset = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
+gray_scaled_image = cv2.cvtColor(img_asset, cv2.COLOR_BGR2GRAY)
 
 corners_cut = cv2.goodFeaturesToTrack(gray_scaled_image, 100, 0.01, 10)
 corners_cut = np.int0(corners_cut)
 
 for corner in corners_cut:
 	x, y = corner.ravel()
-	cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+	cv2.circle(img_asset, (x, y), 5, (255, 0, 0), -1)
 
 for i in range(len(corners_cut)):
 	for j in range(i + 1, len(corners_cut)):
@@ -19,6 +19,6 @@ for i in range(len(corners_cut)):
 		color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
 		cv2.line(img, corner_cut_1, corner_cut_2, color, 1)
 
-cv2.imshow('Frame', img)
+cv2.imshow('Frame', img_asset)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
